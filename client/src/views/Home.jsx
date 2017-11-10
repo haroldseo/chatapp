e9f69ce05041059a0be91ed7e8ff8418a2c7e8b7
Harold Seo <harold.seo.612@gmail.com>
2017-11-09 22:03:38 -0800

    fixed typo

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -8,7 +8,7 @@ class Home extends Component {
 			<div className='Home'>
 				<div className="jumbotron">
 					<h1 className="display-3">ChatApp</h1>
-					<p className="lead">A chatting app, for instant communications on electronic devices. ChatApp users can communicate with all other users on a global chatroom, private 1-on-1, or our very own ChatBot.</p>
+					<p className="lead">A chatting app, for instant communications on electronic devices. ChatApp users can communicate with all other users in a global chatroom, private 1-on-1, or our very own ChatBot.</p>
 					<hr className="my-4"/>
 					<p className="lead">
 						<Link className="btn btn-primary btn-lg" to="/signup">Sign Up Now</Link>

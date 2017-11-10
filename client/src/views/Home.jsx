528602277bc6758ab34b467cec58bec591964eac
Harold Seo <harold.seo.612@gmail.com>
2017-11-09 21:57:09 -0800

    changed home page

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 
 class Home extends Component {
 	render() {
 		return (
 			<div className='Home'>
-				<h1>ChatApp</h1>
+				<div className="jumbotron">
+					<h1 className="display-3">ChatApp</h1>
+					<p className="lead">A chatting app, for instant communications on electronic devices. ChatApp users can communicate with all other users on a global chatroom, private 1-on-1, or our very own ChatBot.</p>
+					<hr className="my-4"/>
+					<p className="lead">
+						<Link className="btn btn-primary btn-lg" to="/signup">Sign Up Now</Link>
+					</p>
+				</div>
 			</div>
 		)
 	}
